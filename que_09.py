ee874395123a51eb4dae5322ddf16f9c207c7954
onkarShweta <smillyshweta@gmail.com>
2023-09-28 21:08:53 +0530

    oops practice

--- que_09.py
+++ que_09.py
@@ -1,2 +1,9 @@
-# euclidean distance between two coordinates 
+# profit & loss 
+cost_price=int(input("enter cost price:"))
+selling_price=int(input("enter selling price:"))
+if cost_price>selling_price:
+    print("losss")
+else:
+    print("profit")    
+
 
